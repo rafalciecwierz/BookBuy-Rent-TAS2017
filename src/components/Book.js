e68295cc7eff440b2291eb5e8c1714f59b4d8dcd
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2017-10-13 21:43:01 +0200

    Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import BookUtils from './BookUtils'
+import BookUtils from './BookUtils';
+import './Book.css'
 
 class Book extends Component {
   render() {
@@ -7,14 +8,15 @@ class Book extends Component {
       <div className="book">
         <BookUtils />
         <div className="book_info">
-          <h1>The littlest bird</h1>
-          <h2>Annie F. Gilbert</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <div className="book_info--content">
+            <h1>The littlest bird</h1>
+            <h2>Annie F. Gilbert</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
         </div>
-        <img className="book_cover"
-          src="https://marketplace.canva.com/MACBT56QCxE/1/0/thumbnail_large/canva-yellow-brown-bird-illustration-little-childrens-book-cover-MACBT56QCxE.jpg"/>
+
       </div>
     );
   }

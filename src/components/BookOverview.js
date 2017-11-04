dc649743ab145564d060720677a12dd0ec240c3b
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2017-11-04 11:56:45 +0100

    overview buttons

--- src/components/BookOverview.js
+++ src/components/BookOverview.js
@@ -10,7 +10,13 @@ class BookOverview extends Component {
         <div className="overview__details" >
           <h2 className="details__title">Cannibals in love</h2>
           <h3 className="details__author">Mike Roberts</h3>
-          <h5 className="details__price">10</h5>
+
+          <div>
+            <h5 className="details__price">10</h5>
+            <span className="details__action">Buy now</span>
+            <span className="details__action">Add to wishlist &#128154;</span>
+          </div>
+
           <p className="details__description">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>

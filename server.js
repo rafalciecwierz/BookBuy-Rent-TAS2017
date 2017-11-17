0f5186203ebdde7f34855b869c5b33ec73098ed5
zuzannaszymanda <32640873+zuzannaszymanda@users.noreply.github.com>
2017-11-17 11:25:35 +0100

    'init2'

--- server.js
+++ server.js
@@ -1,62 +1,85 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-require('dotenv').config();
-// var Comment = require('./model/books');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/Users");
+var db = mongoose.connection;
+
+var routes = require("./routes/index");
+var users = require("./routes/users");
+
+// Init
 var app = express();
-var router = express.Router();
 
-var port = process.env.API_PORT || 3001;
+// Engine
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({defaultLayout:"layout"}));
+app.set("view engine", "handlebars");
 
-mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds163595.mlab.com:63595/book-shop');
-
-app.use(bodyParser.urlencoded({extended: true}));
+// BodyParser Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', ' * ');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  res.setHeader('Cache-Control', 'no - cache');
-  next();
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Express Session
+app.use(session({
+	secret: "secret",
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split("."),
+			root   = namespace.shift(),
+			formParam = root;
+  
+		while(namespace.length) {
+			formParam += "[" + namespace.shift() + "]";
+		}
+		return {
+			param : formParam,
+			msg   : msg,
+			value : value
+		};
+	}
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+	res.locals.success_msg = req.flash("success_msg");
+	res.locals.error_msg = req.flash("error_msg");
+	res.locals.error = req.flash("error");
+	res.locals.user = req.user || null;
+	next();
 });
 
-router.get('/', function(req, res) {
-  res.json({message: 'API Initialized!'});
-});
+app.use("/", routes);
+app.use("/users", users);
 
-//
-// router.route('/books')
-// //retrieve all comments from the database
-//   .get(function(req, res) {
-//   //looks at our Comment Schema
-//   // Book.find(function(err, books) {
-//   //   if (err)
-//   //     res.send(err);
-//   //
-//   //   //responds with a json object of our database comments.
-//   //   res.json(books)
-//   });
-// })
-// //post new comment to the database
-//   .post(function(req, res) {
-//   var book = {};
-//   // var book = new Book();
-//   //body parser lets us use the req.body
-//   book.author = req.body.author;
-//   book.text = req.body.text;
-//   book.save(function(err) {
-//     if (err)
-//       res.send(err);
-//     res.json({
-//       message: 'Book successfully added !'
-//     });
-//   });
-// });
-app.use('/api', router);
+// Set Port
+app.set("port", (process.env.PORT || 3001));
 
-app.listen(port, function() {
-  console.log(`api running on port ${port}`);
+app.listen(app.get("port"), function(){
+	console.log("Server started on port "+app.get("port"));
 });

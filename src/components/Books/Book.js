186fe0c7832391aed3e74df0283b86aed854281e
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2017-12-08 21:20:50 +0100

    functional components

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import BookUtils from './BookUtils';
 
-class Book extends Component {
-
-  render() {
-    let coverImg = {
+function Book(props) {
+  let coverImg = {
       backgroundImage: 'url("../../assets/img/books/undefined.jpg")'
     }
     try {
-      const coverUrl = require("../../assets/img/books/" + this.props.cover) || "../../assets/img/books/undefined.jpg";
+      const coverUrl = require("../../assets/img/books/" + props.cover) || "../../assets/img/books/undefined.jpg";
       coverImg = {
     		backgroundImage: 'url("' + coverUrl + '")'
       }
@@ -18,23 +16,21 @@ class Book extends Component {
 
     return (
       <div className="book">
-        <h5 className="book__price">{this.props.price}</h5>
+        <h5 className="book__price">{props.price}</h5>
 
         <figure className="book__card">
           <canvas className="book__cover"
                   style={coverImg}></canvas>
           <figcaption className="book__details">
-            <p className="details--title">{this.props.title}</p>
-            <p>by {this.props.author}</p>
+            <p className="details--title">{props.title}</p>
+            <p>by {props.author}</p>
           </figcaption>
         </figure>
 
-        <BookUtils tag={this.props.tag}
-                   likes={this.props.likes}/>
+        <BookUtils tag={props.tag}
+                   likes={props.likes}/>
       </div>
-
     );
-  }
 }
 
 export default Book;
--- src/components/Books/BookOverview.js
+++ src/components/Books/BookOverview.js
@@ -1,43 +1,44 @@
 import React, { Component } from 'react';
 
-class BookOverview extends Component {
-  render() {
-    let coverImg = {
-      backgroundImage: 'url("../../assets/img/books/undefined.jpg")'
-    }
-    try {
-      const coverUrl = require("../../assets/img/books/"+this.props.cover) || "../../assets/img/books/undefined.jpg";
-      coverImg = {
-        backgroundImage: 'url("'+coverUrl+'")'
-      }
-    } catch(err) {}
-    const back = () => {window.history.back()}
-    return (
-      <div className="book-overview">
-        <figure className="overview__thumbnails">
-          <canvas className="thumb"
-                  style={coverImg}></canvas>
-        </figure>
-        <div className="overview__details" >
-          <button className="button-back"
-            onClick={back}><i></i></button>
-          <h2 className="details__title">{this.props.title}</h2>
-          <h3 className="details__author">{this.props.author}</h3>
-
-          <div>
-            <h5 className="details__price">{this.props.price}</h5>
-            <span className="details__action">Buy now</span>
-            <span className="details__action">Add to wishlist &#128154;</span>
-          </div>
-
-          <p className="details__description">{this.props.description}          </p>
-          <ul className="details__tags">
-            {this.props.tag}
-          </ul>
-        </div>
-      </div>
-    );
+function BookOverview(props) {
+  let coverImg = {
+    backgroundImage: 'url("../../assets/img/books/undefined.jpg")'
   }
+
+  try {
+    const coverUrl = require("../../assets/img/books/"+props.cover) || "../../assets/img/books/undefined.jpg";
+    coverImg = {
+      backgroundImage: 'url("'+coverUrl+'")'
+    }
+  } catch(err) {}
+
+  const back = () => {window.history.back()}
+
+  return (
+    <div className="book-overview">
+      <figure className="overview__thumbnails">
+        <canvas className="thumb"
+                style={coverImg}></canvas>
+      </figure>
+      <div className="overview__details" >
+        <button className="button-back"
+          onClick={back}><i></i></button>
+        <h2 className="details__title">{props.title}</h2>
+        <h3 className="details__author">{props.author}</h3>
+
+        <div>
+          <h5 className="details__price">{props.price}</h5>
+          <span className="details__action">Buy now</span>
+          <span className="details__action">Add to wishlist &#128154;</span>
+        </div>
+
+        <p className="details__description">{props.description}</p>
+        <ul className="details__tags">
+          {props.tag}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default BookOverview;

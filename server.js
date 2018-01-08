f3fe3eaf618552c1550add8c48173fd2a1af51f7
wercyn <wercyngatorykss@gmail.com>
2018-01-08 09:54:05 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -25,13 +25,10 @@ var port = process.env.API_PORT || 3001;
 
 mongoose.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@ds163595.mlab.com:63595/book-shop");
 
-
-app.use(bodyParser.json({limit: "5mb"}));
-app.use(bodyParser.urlencoded({extended: true, limit: "5mb"}));
 app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.urlencoded({extended: true, limit: "5mb"}));
+app.use(bodyParser.json({limit: "5mb"}));
 app.use(expressValidator({
-	//dozrobienia funkcja validująca czy email i user są w bazie
-
 	errorFormatter: function(param, msg, value) {
 		var namespace = param.split("."), 
 			root    = namespace.shift(), 
@@ -48,10 +45,10 @@ app.use(expressValidator({
 }));
 
 app.use(function(req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", " * ");
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
-	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept");
 	res.setHeader("Cache-Control", "no - cache");
 	next();
 });

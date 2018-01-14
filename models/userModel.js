d4c3e0ac0f4fb0b1df228ddfe5f196cbd49f93ca
wercyn <wercyngatorykss@gmail.com>
2018-01-14 11:09:06 +0100

    Add files via upload

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,30 @@
 var mongoose = require("mongoose");
 var bcrypt = require("bcryptjs");
 var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const mongodbErrorHandler = require("mongoose-mongodb-errors");
+const passportLocalMongoose = require("passport-local-mongoose");
 // User Schema
 
 var UserSchema = new Schema({
 	username: {
 		type: String,
-		index:true
-	},
-	password: {
-		type: String
+		required: "Please supply a name",
+		trim: true
 	},
 	email: {
-		type: String
+		type: String,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		required: "Please Supply an email address"
 	},
-	wishlist: [{type: Schema.ObjectId, ref: 'Book'}],
-	cart: [{type: Schema.ObjectId, ref: 'Book'}]
+	wishlist: [{type: Schema.ObjectId, ref: "Book"}],
+	cart: [{type: Schema.ObjectId, ref: "Book"}]
 });
-var User = module.exports = mongoose.model('User', UserSchema);
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+UserSchema.plugin(mongodbErrorHandler);
+var User = module.exports = mongoose.model("User", UserSchema);
 
 module.exports.createUser = function(newUser, callback){
 	bcrypt.genSalt(10, function(err, salt) {
@@ -27,6 +34,7 @@ module.exports.createUser = function(newUser, callback){
 		});
 	});
 };
+
 
 
 module.exports.getUserByUsername = function(username, callback){
